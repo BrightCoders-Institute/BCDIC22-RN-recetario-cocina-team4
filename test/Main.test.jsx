eba47f5c28d18ea2a91fc5d1961a57e4717a217f
QuackDuster <pablo.jimenezdev@gmail.com>
2023-01-02 22:13:36 -0600

    Fixed Main test for Codecov
    Co-authored-by: victordoom <23017227+victordoom@users.noreply.github.com>
    Co-authored-by: Karen Covarrubias <soykarencm@users.noreply.github.com>
    Co-Authored-By: Enrique Mendoza <henrikhwolf@gmail.com>

--- test/Main.test.jsx
+++ test/Main.test.jsx
@@ -10,6 +10,7 @@ import Main from '../src/views/Main.jsx'
 
 describe('Main view', () =>  {
     it('Has only 2 childs', () => {
+        jest.useFakeTimers();
         const tree = renderer.create(<Main />).toJSON();
         expect(tree.children.length).toBe(2);
     })
